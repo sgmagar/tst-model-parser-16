f33c4b74e6834551ffdaeac0745ffb27cd081eb8
Saurav <sp.gharti@gmail.com>
2023-05-22 13:21:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-model-parser-16",
+  url: "https://tst-model-parser-16.botics.co",
+  api: "https://tst-model-parser-16.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
