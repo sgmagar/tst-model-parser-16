89a28c508bbec39f3b56a76d03a1ebd1efb7b6e3
sgmagar <sp.gharti@gmail.com>
2023-07-19 16:20:52 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,4 +4,3 @@ class Hello(models.Model):
     'Generated Model'
     hiii = models.BigIntegerField(null=True,blank=True,)
     hey = models.BigIntegerField(null=True,blank=True,)
-    ssssdsd = models.BigIntegerField(null=True,blank=True,)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_update_view, user_detail_view, UserDetailView, user_redirect_view, UserUpdateView
+from .views import UserDetailView, user_update_view, UserUpdateView, user_detail_view, user_redirect_view
 
 app_name = "users"
 urlpatterns = [
