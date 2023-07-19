e64ca59f831e3658cf4d31530921ed891e087898
sgmagar <sp.gharti@gmail.com>
2023-07-19 16:13:45 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,5 +2,6 @@ from django.conf import settings
 from django.db import models
 class Hello(models.Model):
     'Generated Model'
-    hiii = models.BigIntegerField(blank=True,null=True,)
+    hiii = models.BigIntegerField(null=True,blank=True,)
     hey = models.BigIntegerField(null=True,blank=True,)
+    ssssdsd = models.BigIntegerField(null=True,blank=True,)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_redirect_view, UserDetailView, UserUpdateView, user_detail_view, user_update_view
+from .views import user_update_view, user_detail_view, UserDetailView, user_redirect_view, UserUpdateView
 
 app_name = "users"
 urlpatterns = [
